9ad201c9b28cd05e69c72dc993fc30a17bb28cd6
Richardson12345 <chuddywarrior@gmail.com>
2018-09-19 10:31:36 +0700

    finished model

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dote = require('dotenv').config();
+mongoose.connect('mongodb://admin:abc123@ds259742.mlab.com:59742/final-project');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+ console.log('connected to final project');
+ // we're connected!
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
